2efaaa81bdd3a20366eda495220dd6e3e298ac66
Margaret Pope <mmp298@gmail.com>
2024-07-22 22:07:15 -0500

    add mongodb uri

--- db/controllers/util/connection.js
+++ db/controllers/util/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.npm_package_config_DB_NAME}`;
-
+const uri = 'mongodb+srv://mmp298:<Cibnaq-3femky-zimvoj>@cluster0.ifhp4sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 // src: https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/lib/dbConnect.js
 
 let cached = global.mongoose
